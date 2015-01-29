f258eb7fc41711bc5b4817dd723669aaa21dde86
Anusha Dasari <anustar93@gmail.com>
2015-01-29 12:20:16 +0530

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StackOverflow::Application.config.secret_key_base = '654e5f6edc3e7eb61537277af705830f0fd68e30474b806e664bdc2180ebe8ecadcef15f29a0b4f366541c736ad1a7147db25087cdf98fcbbe5232455836e78c'
